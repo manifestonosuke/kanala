2427f181a0a9236c042f3a6882cc9af57d652f9c
manifestonosuke <pierrenosuke@gmail.com>
2013-02-07 16:04:45 +0100

    add getops optinos

--- pyweb.py
+++ pyweb.py
@@ -1,4 +1,59 @@
+
+"""
+Get the URL and acces site
+Gives summary of page load and size
+"""
+
+
+
 import urllib2
+import sys
+import getopt 
+
+PRGNAME=sys.argv[0].split('/',1)[1]
+URL='http://onair-pprd.corp.airliquide.com/'
+
+def __print(level,prgname,message,option=""): 
+	print '%30s : %10s : %30s' % (level,prgname,message)
+
+
+def usage():
+	global PRGNAME
+	#print PRGNAME,"usage"
+	#print "debug sys args",sys.argv
+	#for arg in sys.argv:
+	#	print arg
+
+def parseargs(argv):
+	grammar = "kant.xml"
+	global URL
+	#print "Parsing args",argv,"loop"
+	try:
+		opts, args = getopt.getopt(argv, "hu:d", ["help", "url="])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	#print "Parsing opt",opts,"arg",args
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif opt == '-d':
+			global DEBUG
+			DEBUG = 1 
+		elif opt in ("-u", "--url"):
+			print "using URL",URL
+			URL = arg
+
+	remain = "".join(args)
+	#print "remainingargs",remain
+
+if (len(sys.argv) == 1) : 
+	print "empty string"
+	usage()
+	sys.exit(0)
+else:
+	print "argvs",sys.argv
 
 COMMENT='''
 URL='http://nosuke.org/'
@@ -19,8 +74,9 @@ print '---------'
 print data
 '''
 
-URL='http://nosuke.org/'
+parseargs(sys.argv[1:])
 req = urllib2.Request(URL) 
+print "req url",URL
 try:
 	#urllib2.urlopen(req)
 	feed=urllib2.urlopen(req)
